df0a01cb5072cb3e8f67ad52008a9717b0a9a972
mitubhungani <bhunganimitu@gmail.com>
2024-12-30 15:38:31 +0530

    done

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:8090", {
+			const socket = io("https://chat-app-mern-1-gcf1.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
